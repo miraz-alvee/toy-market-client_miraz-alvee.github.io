74ce77d2f411ef222901d21bb6e22dffc7158855
Miraz Alvee <mirazalvee98@gmail.com>
2023-05-21 20:53:33 +0600

    add

--- src/Category/Toyshop.jsx
+++ src/Category/Toyshop.jsx
@@ -5,6 +5,43 @@ import 'react-tabs/style/react-tabs.css';
 
 const Toyshop = () => {
 
+    const [marevel, setMarevel] = useState([]);
+    const [avengers, setAvengers] = useState([]);
+    const [transformer, setTransformer] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        fetch(
+            "https://speedy-nook-server-production.up.railway.app/details/subCategory/sportsCars"
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                setSportsCars(data);
+                setIsLoading(false);
+            });
+    }, []);
+
+    useEffect(() => {
+        fetch(
+            "https://speedy-nook-server-production.up.railway.app/details/subCategory/trucks"
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                setTrucks(data);
+                setIsLoading(false);
+            });
+    }, []);
+
+    useEffect(() => {
+        fetch(
+            "https://speedy-nook-server-production.up.railway.app/details/subCategory/miniFireTrucks"
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                setMiniFireTrucks(data);
+                setIsLoading(false);
+            });
+    }, []);
+
     return (
         <Tabs>
             <TabList className='bg-slate-200 py-3'>
@@ -16,7 +53,7 @@ const Toyshop = () => {
                 <div className="container mx-auto">
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
                         <div className="card card-side bg-base-100 shadow-xl">
-                        <figure><img src="https://i.ibb.co/1ZDgrC2/images-q-tbn-ANd9-Gc-QJm-Yb-BKqofu-To-XC0gpru-Qi28ka-Yb-PXu-IPy-A.jpg" alt="Movie" /></figure>
+                            <figure><img src="https://i.ibb.co/1ZDgrC2/images-q-tbn-ANd9-Gc-QJm-Yb-BKqofu-To-XC0gpru-Qi28ka-Yb-PXu-IPy-A.jpg" alt="Movie" /></figure>
                             <div className="card-body">
                                 <h2 className="card-title">Spiderman</h2>
                                 <p>100</p>
@@ -28,7 +65,7 @@ const Toyshop = () => {
                         </div>
 
                         <div className="card card-side bg-base-100 shadow-xl">
-                        <figure><img src="https://i.ibb.co/jG2JJ3J/images-q-tbn-ANd9-Gc-Rqden-Rs-VDd-Wbn-R3-Sz-WYEgw-C4-KPo6og38-CNw.jpg" alt="Movie" /></figure>
+                            <figure><img src="https://i.ibb.co/jG2JJ3J/images-q-tbn-ANd9-Gc-Rqden-Rs-VDd-Wbn-R3-Sz-WYEgw-C4-KPo6og38-CNw.jpg" alt="Movie" /></figure>
                             <div className="card-body">
                                 <h2 className="card-title">Black Panther</h2>
                                 <p>100</p>
@@ -74,7 +111,7 @@ const Toyshop = () => {
                 <div className="container mx-auto">
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
                         <div className="card card-side bg-base-100 shadow-xl">
-                        <figure><img src="https://i.ibb.co/1Lwx35P/images-q-tbn-ANd9-Gc-RW-jb-Przb-Fs07n491-W0zp-Tlw1nf-PUOPNXc9c-Ypy-Re-Upn-Cd67-Acm-HI0-Of-EId-Yjjb-Z.jpg" alt="Movie" /></figure>
+                            <figure><img src="https://i.ibb.co/1Lwx35P/images-q-tbn-ANd9-Gc-RW-jb-Przb-Fs07n491-W0zp-Tlw1nf-PUOPNXc9c-Ypy-Re-Upn-Cd67-Acm-HI0-Of-EId-Yjjb-Z.jpg" alt="Movie" /></figure>
                             <div className="card-body">
                                 <h2 className="card-title">Hasbo</h2>
                                 <p>100</p>
@@ -86,7 +123,7 @@ const Toyshop = () => {
                         </div>
 
                         <div className="card card-side bg-base-100 shadow-xl">
-                        <figure><img src="https://i.ibb.co/YNPr61h/images-q-tbn-ANd9-Gc-TODHx-IUOuh-FGv9-Ch-D2-MRi-Ngdx-X6-JOGf3-LAXx-PKJ4-Sq-WKThy-Gxdr-Egr-Fyb-RYGK9q.jpg" alt="Movie" /></figure>
+                            <figure><img src="https://i.ibb.co/YNPr61h/images-q-tbn-ANd9-Gc-TODHx-IUOuh-FGv9-Ch-D2-MRi-Ngdx-X6-JOGf3-LAXx-PKJ4-Sq-WKThy-Gxdr-Egr-Fyb-RYGK9q.jpg" alt="Movie" /></figure>
                             <div className="card-body">
                                 <h2 className="card-title">Lukim</h2>
                                 <p>100</p>
